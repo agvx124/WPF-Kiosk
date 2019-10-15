f808fd243d84d43f8f62b346dbe644b675c91e22
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2019-10-15 14:23:30 +0900

    [ADD] Menu Stat Chart
    카테고리별 아직 안함

--- WPF-Kiosk/Control/StatCtrl.xaml.cs
+++ WPF-Kiosk/Control/StatCtrl.xaml.cs
@@ -57,46 +57,77 @@ namespace WPF_Kiosk.Control
 
         public void Load()
         {
-            pieChart1.Series = new SeriesCollection();
+            int[] categoryCount = new int[Enum.GetValues(typeof(Common.eCategory)).Length];
+
+            amountMenuChart.Series = new SeriesCollection();
+            salesMenuChart.Series = new SeriesCollection();
+            amountCategoryChart.Series = new SeriesCollection();
+            salesCategoryChart.Series = new SeriesCollection();
+
+            App.OrderLogData.Where(x => x.food.Category == Common.eCategory.Coffee).Count();
 
             foreach (OrderLog orderLog in App.OrderLogData)
             {
                 if (orderLog.Count != 0)
                 {
-                    PieSeries newItem = new PieSeries();
-                    newItem.Title = orderLog.food.Name;
+                    PieSeries newItem = setPieSeriesItem(orderLog);
+
                     newItem.Values = new ChartValues<int>() { orderLog.Count };
-                    newItem.DataLabels = true;
-                    newItem.LabelPoint = PointLabel;
 
-                    newItem.Style = Resources["PieSeriesStyle"] as Style;
-
-                    pieChart1.Series.Add(newItem);
+                    amountMenuChart.Series.Add(newItem);
                 }
-                
+
+                if (orderLog.Count != 0)
+                {
+                    PieSeries newItem = setPieSeriesItem(orderLog);
+
+                    newItem.Values = new ChartValues<int>() { orderLog.Count * orderLog.food.Price };
+
+                    salesMenuChart.Series.Add(newItem);
+                }
+
+                //switch (orderLog.food.Category)
+                //{
+                //    case Common.eCategory.Coffee:
+                //        categoryCount[0] += orderLog.Count;
+                //    case Common.eCategory.Desert:
+                //        categoryCount[1] += orderLog.Count;
+                //}
+
+                //for (int i = 0; i < orderLog.Count; i++)
+                //{
+                //    if (orderLog.food.Category == Common.eCategory.Coffee)
+                //    {
+                //        categoryCount[0] += orderLog.food.Count;
+                //    }
+                //    else if (orderLog.food.Category == Common.eCategory.Desert)
+                //    {
+                //        categoryCount[1] += orderLog.food.Count;
+                //    }
+                //    else if (orderLog.food.Category == Common.eCategory.Drink)
+                //    {
+                //        categoryCount[2] += orderLog.food.Count;
+                //    }
+                //    else if (orderLog.food.Category == Common.eCategory.SignatureMenu)
+                //    {
+                //        categoryCount[3] += orderLog.food.Count;
+                //    }
+                //}
             }
 
             DataContext = this;
         }
-        //private void setMenuChart()
-        //{
-        //    string[] arr = new string[] { "바나나우유", "딸기우유", "초코우유", "수박우유" };
-        //    pie.Title = "바나나우유";
 
-        //    PointLabel = chartPoint =>
-        //    string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+        private PieSeries setPieSeriesItem(OrderLog orderLog)
+        {
+            PieSeries newItem = new PieSeries();
+            newItem.Title = orderLog.food.Name;
 
-        //    PointName = arr[0];
+            newItem.DataLabels = true;
+            newItem.LabelPoint = PointLabel;
+            newItem.Style = Resources["PieSeriesStyle"] as Style;
 
-        //    DataContext = this;
-        //}
-
-        //private void AddMenuChartItem()
-        //{
-        //   lvcMenu.Series = new SeriesCollection
-        //   {
-        //       foreach ()
-        //   }
-        //}
+            return newItem;
+        }
     }
 }
